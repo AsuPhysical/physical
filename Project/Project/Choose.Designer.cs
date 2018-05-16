0a84148ab63390b27279efd82db5ddb0256ec3cd
DashaMishevna <Dasytka--0912@mail.ru>
2018-05-16 15:01:21 +0400

    Диаграмма

--- Project/Project/Choose.Designer.cs
+++ Project/Project/Choose.Designer.cs
@@ -28,9 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.label3 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // label3
@@ -68,16 +73,36 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(400, 30);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(607, 530);
+            this.chart1.TabIndex = 14;
+            this.chart1.Text = "chart1";
+            // 
             // Choose
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(382, 590);
+            this.ClientSize = new System.Drawing.Size(1050, 590);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.comboBox1);
             this.Name = "Choose";
             this.Text = "Выбор статистики";
+            this.Load += new System.EventHandler(this.Choose_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -88,5 +113,6 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
--- Project/Project/Choose.cs
+++ Project/Project/Choose.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,6 +18,9 @@ namespace Project
             InitializeComponent();
         }
         // ДОПИЛИТЬ ДИАГРАММУ БОЛЬНЫХ/ЗДОРОВЫХ
+        OracleConnection ORACLE = new OracleConnection(constr);
+        static string constr = "User Id=PHYSICAL_PROJECT; Password=1111;Data Source=127.0.0.1:1521/xe";
+        OracleDataAdapter oraAdap = new OracleDataAdapter();
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -35,6 +39,41 @@ namespace Project
             //    (new Statistics()).Show();
             //    this.Hide();
             //}
+
+        }
+
+        private void Choose_Load(object sender, EventArgs e)
+        {
+            ORACLE.Open();
+            chart1.Series[0].Points.Clear();
+            //chart1.Series[0].LegendText = comboBox2.Text;
+
+            List<string> arrX = new List<string>();
+            List<string> arrY = new List<string>();
+
+            //// Статистика по определенному нормативу, за определенный год ВСЕХ ИЛИ Одной группы
+
+            string stat1 = "select STFAM from st_ank1, SP_PHYSICAL_GROUP where SP_PHYSICAL_GROUP.ID = st_ank1.GROUP_ID";
+
+            string stat2 = "select SP_PHYSICAL_GROUP.TITLE from st_ank1, SP_PHYSICAL_GROUP where SP_PHYSICAL_GROUP.ID = st_ank1.GROUP_ID";
+
+
+            OracleCommand oc = new OracleCommand(stat1, ORACLE);
+            OracleDataReader odr = oc.ExecuteReader();
+            //MessageBox.Show(odr.GetInt32(0).ToString);
+            while (odr.Read())
+            {
+                arrX.Add(odr.GetString(0));
+            }
+
+            oc = new OracleCommand(stat2, ORACLE);
+            odr = oc.ExecuteReader();
+            while (odr.Read())
+            {
+                arrY.Add(odr.GetString(0));
+            }
+
+            chart1.Series[0].Points.DataBindXY(arrY, arrX);
 
         }
     }
