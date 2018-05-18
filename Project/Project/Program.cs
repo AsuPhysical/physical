48945d7c1a2602c404d08bc664aa0e18f44ca772
Karina36 <karina.36@mail.ru>
2018-05-18 13:33:08 +0400

    все

--- Project/Project/Program.cs
+++ Project/Project/Program.cs
@@ -18,7 +18,7 @@ namespace Project
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new List());
+            Application.Run(new login());
         }
     }
 }
